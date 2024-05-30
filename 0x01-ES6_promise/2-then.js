f75aad38128c421255d61aae022647b9add05fd5
Annsrutto <annsrutto567@gmail.com>
2024-05-30 05:59:16 +0300

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise.then(() => {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    })
-      .catch(() => {
-        reject(Error());
-      })
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
